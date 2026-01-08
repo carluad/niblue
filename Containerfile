8e2ccc495e5daccf7c5323ef28479a956b573b17
carluad <carluad@proton.me>
2026-01-08 08:44:35 -0300

    Initial commit

---
+++ Containerfile
@@ -0,0 +1,15 @@
+ARG FEDORA_MAJOR_VERSION=43
+
+FROM quay.io/fedora/fedora-silverblue:${FEDORA_MAJOR_VERSION}
+# See https://pagure.io/releng/issue/11047 for final location
+
+# COPY system_files /
+COPY systemd/system /lib/systemd/system
+COPY build.sh /tmp/build.sh 
+
+RUN mkdir -p /var/lib/alternatives && \
+    /tmp/build.sh && \
+    rm -rf /tmp/* /var/* && \
+    mkdir -p /tmp /var/tmp && \
+    chmod -R 1777 /tmp /var/tmp
+
---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+
+# Turtle for nautilus
+dnf install -y python3-pygit2 python3-secretstorage nautilus-python meld
+git clone https://gitlab.gnome.org/philippun1/turtle.git /tmp/turtle
+python /tmp/turtle/install.py install
+
+dnf install -y fastfetch gnome-themes-extra podman-compose podman-docker distrobox
+dnf remove -y firefox firefox-langpacks gnome-software-rpm-ostree toolbx
+
+sed -i '/^PRETTY_NAME/s/Silverblue/Niblue/' /usr/lib/os-release
+
